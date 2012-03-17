e6ce7a0a8835d2c45b95191c7a9f3b22f7328549
George Chigrichenko <georgeevil@gmail.com>
2012-03-17 13:33:25 -0700

    Initlia commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'fb32a914f02e59af9a55ee8c0bde3af5117cb28cce6553b57f45d4475e7d7bfd44ce58a697616036d43346a4cd340512202a1509c2b3e1e11ee3537d8c295538'
